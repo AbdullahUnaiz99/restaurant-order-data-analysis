387e818afb4236c0267fe0872accdf1852812449
AbdullahUnaiz99 <abdlearn99@gmail.com>
2025-12-15 21:07:32 +0530

    analys the order table using sql squery

--- resturant_order.sql
+++ resturant_order.sql
@@ -2,6 +2,7 @@
 use resturant_order;
 go
 
+--------------------------item_table-----------------------
 select * 
 from menu_items
 go
@@ -42,5 +43,40 @@ from menu_items
 group by category 
 go
  
+---------------------------order_table-----------------------
+
+select *
+from order_details
+go
+
+-- what is the date range of the table 
+select min(order_date) as min_date, max(order_date) as max_date
+from order_details
+go
+
+-- How many orders were made within that date range
+select count(distinct order_id) as total_orders 
+from order_details
+go
 
 
+-- how many items were ordered withis this date range 
+select count(order_details_id) as total_items_orders
+from order_details
+go
+
+-- which orders had the most numbers of the items?
+select top 10 order_id, count(item_id) as total_items 
+from order_details
+group by order_id
+order by total_items desc
+go
+
+-- how many orders had the more than 12 items?
+select count(*) as toal_orders_grater_than_12 
+from 
+(select order_id, count(item_id) as num_items
+from order_details
+group by order_id 
+having count(item_id) > 12) AS num_orders
+go
